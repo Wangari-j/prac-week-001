00d463d0504eba26ea4decd7e5edb29280a4f52b
Wangari-j <wangarijudy@gmail.com>
2024-02-05 20:18:27 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,13 @@
+list1 = ["Wandia", "Wairimu"]
+list2  = ["Wandia", "Wairimu"]
+list3 = list2
+
+print(list1 is list2)
+
+print (list2 is list3)
+
+print("wairimu" in list2)
+
+print("Wangari" in list1)
+
+print("wandia" not in list3)
